83b5d8bc1b303faf480cd2120322e9f5439a16d1
Alice-Githui <gnyokabi.alice@gmail.com>
2021-05-21 16:11:36 +0300

    Add user registration, authentication and login

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.homepage, name="homepage")
+    path('', views.index, name="index"),
+    path('login/', views.loginUser, name="loginuser"),
+    path('register/', views.registerUser, name="registeruser"),
+    path('logout/', views.logoutUser, name="logout")
 ]
--- instaclone/views.py
+++ instaclone/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
-def homepage(request):
-    return HttpResponse('Welcome to the IG Clone')
+def index(request):
+    return render(request, 'instaclone/index.html', {})
+
+def registerUser(request):
+    form=RegistrationForm()
+    if request.method == "POST":
+        form=RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('loginuser')
+
+    else:
+        form=RegistrationForm()
+    title="Register New User"
+    return render(request, 'registration/registration.html', {"title":title, "form":form})
+
+def loginUser(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        # print(username)
+        password = request.POST.get('password')
+        # print(password)
+
+        if username and password:
+            user=authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                return redirect('index')
+
+            else:
+                messages.error(request, "Username or Password is incorrect")
+
+        else:
+            messages.error(request, "Field is empty. Enter Username and Password")
+
+    title="Login User"
+    return render(request, 'registration/login.html', {"title":title})
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
+
